71a0d66ded739f813e52f6051c3f75a902624b13
bradynp <bpbraithwaite@gmail.com>
2022-11-27 12:50:52 -0700

    bunch of stuff

--- src/main.cpp
+++ src/main.cpp
@@ -6,7 +6,8 @@
 #include <Wire.h>
 
 #define PIN_OSC 12
-#define TIMEOUT 6000
+#define TIMEOUT 10000
+#define TIMEOUT_SOFT 1000
 #define PIN_SERVO A0
 #define IIC_SDA A4
 #define IIC_SCL A5
@@ -14,8 +15,8 @@
 // set the LCD address to 0x27 for a 16 chars and 2 line display
 LiquidCrystal_I2C lcd(0x27, 16, 2); // LCD Initialization
 
-Servo svo; //motor 
-int pos = 0;
+Servo svo; //motor
+const char *str_default = "---";
 
 const byte ROWS = 4; // four rows
 const byte COLS = 4; // four columns
@@ -26,17 +27,20 @@ char hexaKeys[ROWS][COLS] = {{'1', '2', '3', 'A'},
                              {'*', '0', '#', 'D'}};
 byte rowPins[ROWS] = {9, 8, 7, 6};
 byte colPins[COLS] = {5, 4, 3, 2};
-Keypad kpd = Keypad(makeKeymap(hexaKeys), rowPins, colPins, ROWS, COLS); //keypad setup, defined as kpd object
+Keypad kpd = Keypad(makeKeymap(hexaKeys), rowPins, colPins, ROWS, COLS);
 char k = NO_KEY; //null key
-char valstr[4]; // array of characters to be printed to lcd
-char prevstr[4]; // compare 
+char valstr[4];  // array of characters to be printed to lcd
+char prevstr[4]; // compare
 int idx = 0;
+int idx2 = 0;
 int tar = 0;
 int last = 0; // last motor pos in degrees before buffer
-
+boolean valid = false;
 
 unsigned long tsli = 0; // time since last interaction, ms
 unsigned long toli = 0; // time OF last interaction, ms
+
+inline boolean isNum(char c) { return c >= '0' && c <= '9'; }
 
 void setup() {
     // put your setup code here, to run once:
@@ -48,7 +52,7 @@ void setup() {
     pinMode(PIN_OSC, OUTPUT);
 
     // Object Initializations:
-    kpd.setDebounceTime(10);
+    kpd.setDebounceTime(2);
     //LCD Start initialization
     lcd.init();
     lcd.backlight();
@@ -56,7 +60,7 @@ void setup() {
     lcd.clear();
 
     svo.attach(PIN_SERVO);
-    strcpy(valstr, "000");
+    strcpy(valstr, str_default);
     svo.write(0); //starts at pos 0
 }
 
@@ -64,7 +68,7 @@ void loop() {
     // put your main code here, to run repeatedly:
 
     if ((tsli = millis() - toli) > TIMEOUT) {
-        strcpy(valstr, "000");
+        strcpy(valstr, str_default);
         idx = 0;
     }
 
@@ -80,8 +84,6 @@ void loop() {
         }
     }
 
-    //move cursor to second column
-    //Serial.println(k);
     if (strcmp(prevstr, valstr)) {
         strcpy(prevstr, valstr);
         lcd.setCursor(0, 0);
@@ -91,17 +93,25 @@ void loop() {
         }
         lcd.setCursor(idx, 1);
     }
-    tar = String(valstr).substring(0, 2 + 1).toInt() % 180;
-    if (tar != last && tsli > 10) {
-        last = tar; 
-        svo.write(tar);
-        Serial.println(tar);
+
+    valid = true;
+    for (idx2 = 0; idx2 < 3; ++idx2) {
+        if (!isNum(valstr[idx2])) {
+            valid = false;
+            break;
+        }
     }
-   
+    if (valid) {
+        tar = String(valstr).substring(0, 2 + 1).toInt() % 180;
+        if (tar != last && tsli >= TIMEOUT_SOFT) {
+            last = tar;
+            svo.write(tar);
+            Serial.println(tar);
+        }
+    }
 
     svo.readMicroseconds();
 
     // for graphing
-    delay(1);
     digitalWrite(PIN_OSC, !digitalRead(PIN_OSC));
 }
